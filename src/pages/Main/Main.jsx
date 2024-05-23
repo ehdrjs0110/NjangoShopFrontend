cfe58954d52035a85cb1a952f81f372a29da4ba2
ehdrjs0110 <ehdrjs0110@naver.com>
2024-05-23 15:43:57 +0900

    main add

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,13 +1,49 @@
 import React, { useState , useRef , useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 
 import Navigation from '../../components/Nav/Navigation'
 
+import styles from '../../styles/Main/Main.module.scss'
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function Main() {
+  const navigate = useNavigate();
+
+  const signin = () => {
+    navigate('/SignIn');
+  };
+  
   return (
     <>
         <Navigation></Navigation>
-     
+        <Container fluid className={styles.container}>
+          <div className={styles.main}>
+          <Row className={styles.linkrow}>
+            <Col md={{span: 3, offset: 2}} className={styles.linkbox}>
+              <h1>AI 레시피 검색</h1>
+              <a onClick={signin}>바로가기</a>
+            </Col>
+            <Col md={{span: 3, offset: 2}} className={styles.linkbox}>
+              <h1>냉장고 관리</h1>
+              <a onClick={signin}>바로가기</a>
+            </Col>
+          </Row>
+          <Row className={styles.linkrow}>
+            <Col md={{span: 3, offset: 2}} className={styles.linkbox}>
+              <h1>레시피 공유</h1>
+              <a onClick={signin}>바로가기</a>
+            </Col>
+            <Col md={{span: 3, offset: 2}} className={styles.linkbox}>
+              <h1>갤러리</h1>
+              <a onClick={signin}>바로가기</a>
+            </Col>
+          </Row>
+          </div>
+        </Container>
     </>
   );
 }
--- src/pages/Sign/SignUp.jsx
+++ src/pages/Sign/SignUp.jsx
@@ -5,11 +5,14 @@ import axios from "axios";
 import styles from '../../styles/Sign/SignUp.module.scss';
 
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
 
 import logoImg from '../../assets/Logo/logo.png';
 
 const SignUp = () => {
   const navigate = useNavigate();
+
+  const [isEmail, setEmail] = useState();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -50,23 +53,45 @@ const SignUp = () => {
     //   });
   };
 
+  const sendEmail = () => {
+
+    console.log(isEmail);
+    const data = {
+      emailname : isEmail
+    };
+  
+    //axios 파일 전송
+    axios
+      .post("http://localhost:8080/mail", data)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => {
+        console.log("err message : " + err);
+        alert("메일 보내기 실패");
+      });
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
 
   return (
     <Container fluid className={styles.container}>
       <div className={styles.signup}>
         <img src={logoImg} alt='' className={styles.logoimg} />
         <form onSubmit={handleSubmit} >
-          <div className='inputbox'>
-            <input type="text" id='id' className={styles.inputbox} placeholder='아이디'/>
+          <div className='inputbox'>  
+            <input type="email" id='email' className={styles.inputbox} onChange={handleEmail} placeholder='이메일'/>
+          </div>
+          <div className='email'>
+          <Button variant="primary" className={styles.emailbtn} onClick={sendEmail}>인증번호 전송</Button>
           </div>
           <div className='inputbox'>  
             <input type="password" id='password' className={styles.inputbox} placeholder='비밀번호'/>
           </div>
           <div className='inputbox'>  
             <input type="text" id='name' className={styles.inputbox} placeholder='이름'/>
-          </div>
-          <div className='inputbox'>  
-            <input type="email" id='email' className={styles.inputbox} placeholder='이메일'/>
           </div>
           <div className='inputbox'>  
             <input type="text" id='phone' className={styles.inputbox} placeholder='전화번호'/>
