3ed720a898869b43525c1ceea264104836ecbc69
ehdrjs0110 <ehdrjs0110@naver.com>
2024-06-03 19:38:03 +0900

    Inven delete

--- src/pages/Inven/Inven.jsx
+++ src/pages/Inven/Inven.jsx
@@ -22,8 +22,17 @@ function Inven() {
   const [isChange, setChange] = useState(false);
   //재료 데이터
   const [isData, setData] = useState([]);
+  //추가 재료 데이터
+  const [isNewData, setNewData] = useState([]);
+  //삭제 재료 데이터
+  const [isDelData, setDelData] = useState([]);
 
 
+  useEffect(() => {
+    setNewData({      
+      userid : "ehdrjs0110",
+    });
+  },[]);
 
   useEffect(() => {
 
@@ -34,7 +43,7 @@ function Inven() {
       try{
         const res = await axios.get("http://localhost:8080/inven/manage", {params});
         console.log(res.data);
-        setData(res.data);
+        setData(res.data);        
 
       }catch(err){
         console.log("err message : " + err);
@@ -47,27 +56,73 @@ function Inven() {
 
   const addData = async () => {
 
-    const data = { 
-      userid : "ehdrjs0110",
-      ingredientname : "오이",
-      size : "없음",
-      count : 0,
-      dateofuse :"",
-      lastuse :"",
-      lastget :"",
-      memo : ""
-    };
+    const data = isNewData;
 
     try{
       console.log(data);
-      const res = await axios.post("http://localhost:8080/inven/manage/add", {data});
-      console.log(res.data);
-      setData(res.data);
+      const res = await axios.post("http://localhost:8080/inven/manage/add", data, {
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
       setChange(!isChange);
 
     }catch(err){
       console.log("err message : " + err);
     }
+
+  };
+
+  //재료 삭제
+  const deleteData = async (selectIndex) => {
+
+    const selectedItem = isData[selectIndex];
+    setDelData(selectedItem.ingredientname);
+
+    const params = {
+      userid: "ehdrjs0110",
+      ingredientname: selectedItem.ingredientname
+    };
+
+    if(window.confirm(`정말 ${selectedItem.ingredientname}를 삭제하시겠습니까?`)){
+      try{ 
+        console.log(params);
+        await axios.delete("http://localhost:8080/inven/manage/delete", {params});
+        alert("삭제 되었습니다.");
+        setChange(!isChange);
+      }catch(err){
+        console.log("err message : " + err);
+      }
+    }else {
+      alert("취소 되었습니다.");
+    }
+    
+
+  };
+
+  //재료명 입력
+  const setIngredName = (e) => {
+    setNewData((isNewData) => ({
+      ...isNewData,
+      "ingredientname" : e.target.value,
+    }));
+  };
+
+  //재료양 입력
+  const setSize = (e) => {
+    setNewData((isNewData) => ({
+      ...isNewData,
+      "size" : e.target.value,
+    }));
+
+  };
+  
+  //재료수량 입력
+  const setCount = (e) => {
+    setNewData((isNewData) => ({
+      ...isNewData,
+      "count" : e.target.value,
+    }));
 
   };
 
@@ -103,6 +158,28 @@ function Inven() {
           <Row className={styles.contentRow}>
             <Col md={{span: 10, offset: 1}} className={styles.content}>
 
+            <Row className={styles.addline}>
+                <Col>
+                <Form.Control type="text" className={styles.ingredientname} onChange={setIngredName} placeholder="재료명"/>
+                </Col>
+                <Col>
+                  <Button className={styles.btn} variant="secondary" onClick={setSize} value={"없음"} >없음</Button>
+                </Col>
+                <Col>
+                  <Button className={styles.btn} variant="warning" onClick={setSize} value={"적음"} >적음</Button>
+                  <Button className={styles.btn} variant="primary" onClick={setSize} value={"적당함"} >적당함</Button>
+                  <Button className={styles.btn} variant="success" onClick={setSize} value={"많음"} >많음</Button>
+                </Col>
+                <Col>
+                  <p className={styles.text}>수량</p>
+                  <Form.Control type="number" className={styles.count} onChange={setCount} placeholder="0"/>
+                </Col>
+                <Col>
+                <Button className={styles.btn} variant="primary" onClick={addData}>추가</Button>
+                </Col>
+              </Row>
+
+
               {isData.map((item, index) => {
                 // 클래스 네임 결합
                 const combinedClassName = classNames(
@@ -129,14 +206,12 @@ function Inven() {
                         <Form.Control type="number" className={styles.count} placeholder="0" value={item.count} />
                       </Col>
                       <Col>
-                        <Button className={styles.btn} variant="danger">삭제</Button>
+                        <Button className={styles.btn} onClick={() => deleteData(index)} variant="danger">삭제</Button>
                       </Col>
                     </Row>
                   </div>
                 );
               })}
-
-              <Button variant="primary" onClick={addData}>+</Button>
 
             </Col>
           </Row>
