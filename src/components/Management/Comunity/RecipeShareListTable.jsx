2b1b18bc0ad34ebfa2fbba3760d8693a2df9979d
명재 유 <alkjfgh@gmaili.com>
2024-08-31 20:47:12 +0900

    recipeShare 끝.

--- src/components/Management/Comunity/RecipeShareListTable.jsx
+++ src/components/Management/Comunity/RecipeShareListTable.jsx
@@ -1,19 +1,31 @@
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import TableWithPagination from "../../Table/TableWithPagination";
+import { axiosInstance } from "../../../middleware/customAxios";
 
 const RecipeShareListTable = () => {
+    const [reloadTrigger, setReloadTrigger] = useState(false); // 데이터를 다시 로드하기 위한 트리거 상태
 
-    const handleAction = (recipeShareID) => {
-        console.log(`Edit recipeShare with ID: ${recipeShareID}`);
+    const handleDelete = (recipeShareID) => {
+        console.log(`Delete recipeShare with ID: ${recipeShareID}`);
+        axiosInstance.delete(`management/recipeShare/deleteRecipeShareByRecipeShareID/${recipeShareID}`)
+            .then(response => {
+                console.log(response.data);
+                setReloadTrigger(!reloadTrigger); // 삭제 후 reloadTrigger를 변경하여 데이터를 다시 로드
+            })
+            .catch(error => {
+                console.error("There was an error deleting the recipeShare!", error);
+            });
     };
 
     const columns = [
-        { header: 'recipe_share_id' },
-        { header: 'recipe_id' },
-        { header: 'user_id' },
-        { header: 'like_count' },
+        { header: 'recipeShareId' },
+        { header: 'recipeId' },
+        { header: 'userId' },
+        { header: 'likeCount' },
         { header: 'update' },
-        { header: 'create_at' },
+        { header: 'createAt' },
+        { header: 'actions' },
     ];
 
     const renderRecipeShareRow = (recipeShare, index) => {
@@ -22,14 +34,14 @@ const RecipeShareListTable = () => {
 
         return (
             <tr key={index} className={rowClass}>
-                <td>{recipeShare.recipeShare_id}</td>
-                <td>{recipeShare.recipe_id}</td>
-                <td>{recipeShare.user_id}</td>
-                <td>{recipeShare.like_count}</td>
+                <td>{recipeShare.recipeShareId}</td>
+                <td>{recipeShare.recipeId}</td>
+                <td>{recipeShare.userId}</td>
+                <td>{recipeShare.likeCount}</td>
                 <td>{new Date(recipeShare.update).toLocaleString()}</td>
-                <td>{new Date(recipeShare.create_at).toLocaleString()}</td>
+                <td>{new Date(recipeShare.createAt).toLocaleString()}</td>
                 <td>
-                    <Button variant="danger" size="sm" onClick={() => handleAction(recipeShare.id)}>
+                    <Button variant="danger" size="sm" onClick={() => handleDelete(recipeShare.recipeShareId)}>
                         handle
                     </Button>
                 </td>
@@ -38,13 +50,12 @@ const RecipeShareListTable = () => {
     };
 
     return (
-        <></>
-        // <TableWithPagination
-        //     apiEndpoint="management/user/getUserListbyIndex"
-        //     columns={columns}
-        //     renderRow={renderRecipeShareRow}
-        //     pageSize={5}
-        // />
+        <TableWithPagination
+            apiEndpoint="management/recipeShare/getRecipeShareListByIndex"
+            columns={columns}
+            renderRow={renderRecipeShareRow}
+            pageSize={5}
+        />
     );
 };
 
