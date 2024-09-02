009a25e259a5aa321ee9cdee83c72052b210757d
ehdrjs0110 <ehdrjs0110@naver.com>
2024-09-02 18:44:15 +0900

    resetAll

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-// import '../src/styles/index.css';
 import { CookiesProvider } from 'react-cookie';
 import store from './Store/store'
 import { Provider } from 'react-redux'
